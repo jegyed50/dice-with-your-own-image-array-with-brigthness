39bb5979f27b550596399433a18a0639f4c03cb8
Egyed József <jegyed50@gmail.com>
2023-05-24 08:09:02 +0200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -17,9 +17,40 @@ function MyImageShow (pMyImage: number[]) {
 }
 let CurrentBrigthness = 0
 let dice = 0
+let MyImage2: number[] = []
 let MyImage: number[] = []
-let list = [MyImage]
+let MyImageList = [MyImage, MyImage2]
+// let MyImageList:  [] []
 MyImage = [
+0,
+10,
+20,
+30,
+40,
+50,
+60,
+70,
+80,
+90,
+100,
+110,
+120,
+130,
+140,
+150,
+160,
+170,
+180,
+190,
+200,
+210,
+220,
+230,
+240
+]
+MyImage2 = [0, 1]
+// let MyImageList:  [] []
+MyImage2 = [
 0,
 10,
 20,
