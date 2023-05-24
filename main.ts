8433a6faec41ba25d79eb8c7ceebd686b00cd81f
Egyed József <jegyed50@gmail.com>
2023-05-24 07:45:13 +0200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,50 +1,45 @@
 input.onButtonPressed(Button.A, function () {
     dice = randint(1, 6)
-    list[dice - 1].showImage(0)
 })
+input.onButtonPressed(Button.B, function () {
+	
+})
+function MyImageShow () {
+    for (let MyColum = 0; MyColum <= 4; MyColum++) {
+        for (let MyRow = 0; MyRow <= 4; MyRow++) {
+            CurrentBrigthness = MyImage[MyColum * 4 + MyRow]
+            led.plotBrightness(MyColum, MyRow, CurrentBrigthness)
+        }
+    }
+}
+let CurrentBrigthness = 0
 let dice = 0
-let list: Image[] = []
-list = [
-images.createImage(`
-    . . . . .
-    . . . . .
-    . . # . .
-    . . . . .
-    . . . . .
-    `),
-images.createImage(`
-    . . . . .
-    . . # . .
-    . . . . .
-    . . # . .
-    . . . . .
-    `),
-images.createImage(`
-    . . . . #
-    . . . . .
-    . . # . .
-    . . . . .
-    # . . . .
-    `),
-images.createImage(`
-    . . . . .
-    . # . # .
-    . . . . .
-    . # . # .
-    . . . . .
-    `),
-images.createImage(`
-    # . . . #
-    . . . . .
-    . . # . .
-    . . . . .
-    # . . . #
-    `),
-images.createImage(`
-    . # . # .
-    . . . . .
-    . # . # .
-    . . . . .
-    . # . # .
-    `)
+let MyImage: number[] = []
+let list = [MyImage]
+MyImage = [
+0,
+10,
+20,
+30,
+40,
+50,
+60,
+70,
+80,
+90,
+100,
+110,
+120,
+130,
+140,
+150,
+160,
+170,
+180,
+190,
+200,
+210,
+220,
+230,
+240
 ]
